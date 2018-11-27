2d4dfe17d3336c00876eef0509da6d57f90cdf06
Ricardo Robson <rcrdrobson@gmail.com>
2018-11-27 10:18:26 -0300

    Init parser

---
+++ parser.hs
@@ -0,0 +1,7 @@
+module ParseWhile where
+import System.IO
+import Control.Monad
+import Text.ParserCombinators.Parsec
+import Text.ParserCombinators.Parsec.Expr
+import Text.ParserCombinators.Parsec.Language
+import qualified Text.ParserCombinators.Parsec.Token as Toke
